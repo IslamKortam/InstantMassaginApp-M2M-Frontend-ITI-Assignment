fab93e3211040231db20f8af38ad8aa60f6b717c
Islam Kortam <eng.islam.kortam@gmail.com>
2022-06-07 02:12:01 +0200

    Implementing handleSubmit to handle sending a msg

--- src/components/ChatApp/ChatArea/ChatControllers/ChatControllers.jsx
+++ src/components/ChatApp/ChatArea/ChatControllers/ChatControllers.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
+
 export default function ChatControllers(){
-    
+    const [newMsgInputField, setNewMsgInputField] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newMsg = newMsgInputField;
+        if(newMsg === ''){
+            //Early Return
+            return;
+        }
+        console.log(newMsg);
+        setNewMsgInputField('');
+    }
+
     return(
         <div className="ChatControllers">
             <form action="">
-                <input type="text" className="msgInputText"/>
-                <input type="submit" value="Send" className="msgSendButton"/>
+                <input type="text" className="msgInputText" value={newMsgInputField} onChange={(e) => setNewMsgInputField(e.target.value)}/>
+                <input type="submit" value="Send" className="msgSendButton" onClick={handleSubmit}/>
             </form>
         </div>
     );
